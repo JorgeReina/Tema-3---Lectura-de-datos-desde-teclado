b8b1b31951d1a68d73ed1888012e4c6c171269b5
Jorge Reina Romero <116582548+JorgeReina@users.noreply.github.com>
2022-11-10 17:52:27 +0100

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,28 @@
+/**
+*
+*	Ejercicio 2, Conversor de euros a pesetas, los euros se introducen por teclado.
+* 
+*	Nombre del archivo: Ejercicio1.java
+*
+*	@author Jorge Reina Romero
+*
+*/
+
+public class Ejercicio2 {
+	public static void main(String[] args) {
+		
+		int pesetas = 166;
+		
+		System.out.println("********************************");
+		System.out.println("* Conversor de Euros a Pesetas *");
+		System.out.println("********************************");
+		
+		System.out.print("Introduzca la cantidad de Euros que desea cambiar: ");
+		int euros = Integer.parseInt( System.console().readLine() );
+		
+		int conversor = euros * pesetas;
+		
+		System.out.println("Los '" + euros + "' euros son '" + conversor + "' pesetas.");
+		
+	}
+}
